be69644dda955dc151c8e31958ca3e49b0d3cf99
Falldio <1031249495@qq.com>
2023-04-17 19:44:33 +0800

    add more contacts info

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,9 +22,22 @@ module.exports = {
                 {
                     type: 'github',
                     link: 'https://github.com/Falldio',
+                },
+                {
+                    type: 'linkedin',
+                    link: 'https://www.linkedin.cn/incareer/in/ACoAACpuYSYBUOXa-c2x79JYFfhEtRgRexrSBO4',
+                },
+                {
+                    type: 'twitter',
+                    link: 'https://twitter.com/lichng79348303'
                 }
             ]
-        }
+        },
+        nav: [
+            { text: 'Blogs', link: '/' },
+            { text: 'Tags', link: '/tag/' },
+            { text: 'Now', link: '/now/' },
+        ],
     },
     markdown: {
         extendMarkdown: md => {
