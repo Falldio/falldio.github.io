21a5212c2ce704241befe79e686e2e052b84e636
falldio <1031249495@qq.com>
2024-04-07 21:28:33 +0800

    增加卡片标题高度

--- docs/.vitepress/theme/components/HomeCard.vue
+++ docs/.vitepress/theme/components/HomeCard.vue
@@ -10,9 +10,9 @@ const props = defineProps({
 <template>
     <a :href="url" class="no-underline">
         <article
-            class="home-card group home-card px-10 py-8 bg-green-10 shadow-xl transition ease-in-out delay-150 hover:bg-white hover:cursor-pointer duration-300 ">
+            class="home-card group home-card px-10 py-8 bg-green-10 shadow-xl transition ease-in-out delay-150 hover:bg-white hover:cursor-pointer duration-300 h-full">
             <h3
-                class="home-card-title mb-8 font-black text-2xl transition ease-in-out delay-150 group-hover:text-kokura-accent duration-300">
+                class="home-card-title mb-8 font-black text-2xl transition ease-in-out delay-150 group-hover:text-kokura-accent duration-300 line-clamp-2 h-16">
                 {{
         title }}
             </h3>
