98fb1d18fca752ffe5c8aaf333c2902b9ce31c45
falldio <1031249495@qq.com>
2024-06-09 23:59:41 +0800

    responsive design

--- docs/.vitepress/theme/components/AllTags.vue
+++ docs/.vitepress/theme/components/AllTags.vue
@@ -6,7 +6,7 @@ import TagButton from './TagButton.vue';
 <template>
     <div>
         <h1 class="page-header text-4xl font-bold py-10 border-dashed border-b">{{ $frontmatter.title }}</h1>
-        <div class="tags-container flex flex-wrap gap-1 w-3/4">
+        <div class="tags-container flex flex-wrap gap-1 w-3/4 max-md:w-full">
             <TagButton v-for="tag in Object.keys(tags)" :key="tag" :tag="tag" :displayText="tag + ': ' + tags[tag].length"/>
         </div>
     </div>
--- docs/.vitepress/theme/components/Now.vue
+++ docs/.vitepress/theme/components/Now.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="now w-4/5">
+    <div class="now w-4/5 max-md:w-full">
         <div class="page-header py-10 border-dashed border-b">
             <h1 class="text-4xl font-bold">{{ $frontmatter.title }}</h1>
             <time class="text-lg text-gray-500 block mt-5" :datetime="$frontmatter.date" >{{  $frontmatter.date }}</time>
